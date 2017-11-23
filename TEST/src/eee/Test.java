63b0c30a34a2669fd11f4e5866f79508ff3cc53e
mouse <503673140@qq.com>
2017-11-23 16:30:02 +0800

    new commit

--- TEST/src/eee/Test.java
+++
@@ -1,8 +0,0 @@
-package eee;
-import java.util.Random;
-import java.math.BigDecimal;
-public class Test{
-	public static void main(String[]arg){
-		
-	}
-}
--- ant colony optimization/src/antColony/Ordinal.java
+++
@@ -1,4 +0,0 @@
-package antColony;
-public class Ordinal {
-
-}
